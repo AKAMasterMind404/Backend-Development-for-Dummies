e89cea4ac0fc7e03cc5ce7344275d2394d7cd2a8
Atharv Karbhari <59548859+AKAMasterMind404@users.noreply.github.com>
2022-05-28 15:04:24 +0530

    express init

--- server.js
+++ server.js
@@ -1,41 +1,9 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
 
-const server = http.createServer((req,res)=>{
+const app = express()
 
-    let siteName = './abc.html';
+app.listen(3000)
 
-    switch(req.url){
-        case '/about':
-            res.statusCode = 200;
-            siteName = './about.html';
-            break;
-        case '/about-me':
-            res.statusCode = 301;
-            res.setHeader('Location','/about');
-            break;
-        case '/home':
-            res.statusCode = 200;
-            siteName = './abc.html';
-            break;
-        default:
-            res.statusCode = 404;
-            siteName = './error.html';
-            break;
-    }
-
-    console.log("Server!!");
-    fs.readFile(siteName, (err, data)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.write(data);
-        }
-        res.end();
-    })
-}); 
-
-server.listen(3000, 'localhost',()=>{
-    console.log("Listening on 3000");
+app.get('/', (req, res)=>{
+    res.send("Hey");
 });
