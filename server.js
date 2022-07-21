8f96ce2f51af3ac2c3d7288eb30cdc731175a5bf
Atharv Karbhari <59548859+AKAMasterMind404@users.noreply.github.com>
2022-07-21 15:55:01 +0530

    update1

--- server.js
+++ server.js
@@ -1,9 +1,62 @@
 const express = require('express');
+var bodyParser = require('body-parser')
 
 const app = express()
 
-app.listen(3000)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.get('/', (req, res)=>{
-    res.send("Hey");
-});
+let userMap = {};
+
+function getUsers(req,res){
+    
+    return res.json({
+        "users":userMap        
+    })
+}
+
+function getUserById(req,res){
+    return res.json({
+        "message": "User with ID!"
+    })
+}
+
+function updateUser(req,res){
+    return res.json({
+        "message": "Updated User!"
+    })
+}
+
+function deleteUser(req,res){
+    return res.json({
+        "message": "Deleted User!"
+    })
+}
+
+function addUser(req,res){
+    console.log(req.body.name);
+    return res.json({
+        "message": "Added User!"
+    })
+}
+
+app.listen(3000, (req,res)=>{
+    console.log("Started on port 3000!");
+})
+
+const userRouter = express.Router();
+app.use('/user', userRouter);
+
+userRouter
+    .route('/')
+    .get(getUsers)
+    .post(addUser)
+    .delete(deleteUser)
+    .patch(updateUser)
+
+userRouter
+    .route('/:id').get(getUserById)
+
+app.use((req,res)=>{
+    res.status(404).sendFile('./error.html', {root: __dirname});
+})
